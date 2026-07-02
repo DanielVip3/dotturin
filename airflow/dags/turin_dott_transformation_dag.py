f6f94b054e67c86ab9a9ccdb183bde5dcf205ad7
Daniele De Martino <danieledemartino.72004@gmail.com>
2026-07-02 19:45:09 +0200

    Feat: add Airflow task to run gold transformation every 30 minutes
    Alongside silver transformation

--- airflow/dags/turin_dott_transformation_dag.py
+++ airflow/dags/turin_dott_transformation_dag.py
@@ -3,14 +3,14 @@ from airflow.sdk import dag
 from airflow.providers.apache.spark.operators.spark_submit import SparkSubmitOperator
 
 @dag(
-  dag_id='turin_dott_silver_transformation',
+  dag_id='turin_dott_transformation',
   default_args=default_args,
-  description='Batch transformation (bronze to silver) every 30 minutes',
+  description='Batch transformation (bronze to silver and silver to gold) every 30 minutes',
   schedule='*/30 * * * *',
   start_date=start_date,
   catchup=False
 )
-def turin_dott_silver_dag():
+def turin_dott_transformation_dag():
   run_silver = SparkSubmitOperator(
     task_id='run_silver',
     application='/opt/airflow/scripts/transform_silver.py',
@@ -21,4 +21,16 @@ def turin_dott_silver_dag():
     verbose=True
   )
 
-turin_dott_silver_dag()
+  run_gold = SparkSubmitOperator(
+    task_id='run_gold',
+    application='/opt/airflow/scripts/transform_gold.py',
+    conn_id='spark_default',
+    conf={
+      'spark.cores.max': SPARK_APP_CORES
+    },
+    verbose=True
+  )
+
+  run_silver >> run_gold
+
+turin_dott_transformation_dag()
