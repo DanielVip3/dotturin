9200eafcf190aaed9b3afe1819e4147d9febcf37
Daniele De Martino <danieledemartino.72004@gmail.com>
2026-07-03 14:23:34 +0200

    Refactor: change bronze layer logic to silver layer logic. Temporarily removed gold layer.
    The codebase is undergoing a big structural change

--- airflow/dags/turin_dott_transformation_dag.py
+++ airflow/dags/turin_dott_transformation_dag.py
@@ -5,32 +5,22 @@ from airflow.providers.apache.spark.operators.spark_submit import SparkSubmitOpe
 @dag(
   dag_id='turin_dott_transformation',
   default_args=default_args,
-  description='Batch transformation (bronze to silver and silver to gold) every 30 minutes',
+  description='Batch transformation (silver to gold) every 30 minutes',
   schedule='*/30 * * * *',
   start_date=start_date,
   catchup=False
 )
 def turin_dott_transformation_dag():
-  run_silver = SparkSubmitOperator(
-    task_id='run_silver',
-    application='/opt/airflow/scripts/transform_silver.py',
-    conn_id='spark_default',
-    conf={
-      'spark.cores.max': SPARK_APP_CORES
-    },
-    verbose=True
-  )
-
-  run_gold = SparkSubmitOperator(
-    task_id='run_gold',
-    application='/opt/airflow/scripts/transform_gold.py',
-    conn_id='spark_default',
-    conf={
-      'spark.cores.max': SPARK_APP_CORES
-    },
-    verbose=True
-  )
-
-  run_silver >> run_gold
+  # Temporarily disabled as gold is not up-to-date.
+  #run_gold = SparkSubmitOperator(
+  #  task_id='run_gold',
+  #  application='/opt/airflow/scripts/transform_gold.py',
+  #  conn_id='spark_default',
+  #  conf={
+  #    'spark.cores.max': SPARK_APP_CORES
+  #  },
+  #  verbose=True
+  #)
+  pass
 
 turin_dott_transformation_dag()
