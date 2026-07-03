b5624cdb53fb37af324cb27bdfe035d37cb5d79c
Daniele De Martino <danieledemartino.72004@gmail.com>
2026-07-03 15:09:26 +0200

    Fix: change optimization script to respect new bronze layer partitioning logic

--- scripts/optimize_delta.py
+++ scripts/optimize_delta.py
@@ -15,28 +15,23 @@ def main():
   spark = get_spark_session(f"DotTurinOptimize-{layer}")
 
   if layer == "bronze":
-    print("[*] Optimizing bronze layer...")
-    
-    # 128 MB = 134.217.728 bytes
-    spark.sql("ALTER TABLE delta.`s3a://dotturin-raw/bikes/` SET TBLPROPERTIES ('delta.targetFileSize' = '134217728')")
-    
-    spark.sql("OPTIMIZE delta.`s3a://dotturin-raw/bikes/`")
-    print("[+] Bronze optimized successfully.")
-
-  elif layer == "silver":
     if len(sys.argv) != 5:
       print("[!] Error: year, month and day required.")
       sys.exit(1)
         
     year, month, day = sys.argv[2], sys.argv[3], sys.argv[4]
     
-    print(f"[*] Optimizing silver layer on date Year={year} Month={month} Day={day}...")
+    print(f"[*] Optimizing bronze layer on date Year={year} Month={month} Day={day}...")
 
     # 512 MB = 536.870.912 bytes
-    spark.sql("ALTER TABLE delta.`s3a://dotturin-processed/bikes_status/` SET TBLPROPERTIES ('delta.targetFileSize' = '536870912')")
+    spark.sql("ALTER TABLE delta.`s3a://dotturin-raw/bikes/` SET TBLPROPERTIES ('delta.targetFileSize' = '536870912')")
     
-    spark.sql(f"OPTIMIZE delta.`s3a://dotturin-processed/bikes_status/` WHERE year = {year} AND month = {month} AND day = {day}")
-    print(f"[+] Silver optimized successfully for Year={year} Month={month} Day={day}.")
+    spark.sql(f"OPTIMIZE delta.`s3a://dotturin-raw/bikes/` WHERE year = {year} AND month = {month} AND day = {day}")
+    print(f"[+] Bronze optimized successfully for Year={year} Month={month} Day={day}.")
+
+  elif layer == "silver":
+    # TO REWRITE
+    pass
 
   spark.stop()
 
