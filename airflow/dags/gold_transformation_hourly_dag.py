301b97f1252f74bef00d0bceb4a6c02221d689ac
Daniele De Martino <danieledemartino.72004@gmail.com>
2026-07-10 21:53:01 +0200

    Feat: add Soda data quality validation for silver and gold data layers
    Validation runs automatically every hour and prevents from writing in gold layer if incorrect data is found in silver; viceversa, at the moment, no action is done (except notification) if incorrect data is found in gold layer

--- airflow/dags/gold_transformation_hourly_dag.py
+++ airflow/dags/gold_transformation_hourly_dag.py
@@ -1,5 +1,6 @@
 from common import SPARK_APP_CORES, start_date, default_args
 from airflow.sdk import dag
+from airflow.providers.standard.operators.bash import BashOperator
 from airflow.providers.apache.spark.operators.spark_submit import SparkSubmitOperator
 
 
@@ -12,7 +13,19 @@ from airflow.providers.apache.spark.operators.spark_submit import SparkSubmitOpe
   catchup=False,
 )
 def gold_hourly_transformation():
-  SparkSubmitOperator(
+  # Validate the silver Delta Lake input data (to prevent writing incorrect data to gold)
+  # We use a Spark job as it has to read from Delta Lake
+  validate_silver = SparkSubmitOperator(
+    task_id="validate_silver",
+    application="/opt/airflow/scripts/silver/validate.py",
+    py_files="/opt/airflow/scripts/common.py",
+    conn_id="spark_default",
+    conf={"spark.cores.max": SPARK_APP_CORES},
+    verbose=True,
+  )
+
+  # Run Spark job to populate ClickHouse data warehouse
+  run_gold = SparkSubmitOperator(
     task_id="run_gold",
     application="/opt/airflow/scripts/gold/clickhouse_hourly.py",
     py_files="/opt/airflow/scripts/common.py",
@@ -20,6 +33,14 @@ def gold_hourly_transformation():
     conf={"spark.cores.max": SPARK_APP_CORES},
     verbose=True,
   )
+
+  # Validate the gold data
+  validate_gold = BashOperator(
+    task_id="validate_gold",
+    bash_command="soda scan -d clickhouse -c /opt/soda/configuration.yml /opt/soda/checks_gold.yml",
+  )
+
+  validate_silver >> run_gold >> validate_gold
 
 
 gold_hourly_transformation()
---
+++ scripts/silver/validate.py
@@ -0,0 +1,38 @@
+from common import get_spark_session
+from soda.scan import Scan
+
+
+def main():
+  spark = get_spark_session("TwitchNoNameSodaSilverValidation")
+
+  streams_df = spark.read.format("delta").load("s3a://twitch-silver/streams/")
+  tags_df = spark.read.format("delta").load("s3a://twitch-silver/stream_tags/")
+  transitions_df = spark.read.format("delta").load("s3a://twitch-silver/stream_transitions/")
+  games_df = spark.read.format("delta").load("s3a://twitch-silver/games/")
+
+  # Register tables as temporary views so Soda can query them
+  streams_df.createOrReplaceTempView("streams")
+  tags_df.createOrReplaceTempView("stream_tags")
+  transitions_df.createOrReplaceTempView("stream_transitions")
+  games_df.createOrReplaceTempView("games")
+
+  # Initialize Soda scan
+  scan = Scan()
+  scan.set_verbose(True)
+  scan.set_scan_definition_name("Silver validation")
+  scan.set_data_source_name("spark_df")
+
+  # Attach Spark session and YAML file
+  scan.add_spark_session(spark)
+  scan.add_sodacl_yaml_file("/opt/soda/checks_silver.yml")
+
+  scan.execute()
+  print(scan.get_logs_text())
+
+  # Fail the job if data quality is compromised
+  if scan.has_error_or_warning_logs():
+    raise Exception(f"Data quality checks failed for silver layer! Message: {scan.get_error_or_warning_logs_text()}")
+
+
+if __name__ == "__main__":
+  main()
